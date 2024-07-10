d710b4c3201af504ef1c8372728ebc2689d8a66b
Ben Pitzer <ben.pitzer@gmail.com>
2024-07-10 11:35:31 -0800

    Integrate state updates into light controller

--- light_controller/core/pattern_generator.py
+++ light_controller/core/pattern_generator.py
@@ -15,14 +15,13 @@ class PatternGenerator:
 
     def __init__(self, state, head_configs, args):
         self._installation_state = state
-        self._previous_installation_state = copy.deepcopy(state)
         self._args = args
         self._results = asyncio.Future()
         self._head_configs = head_configs
-        self._animation_time_delta = 1.0 / args.animation_rate
-        self._cur_animation_time = time.time()
-        self._next_animation_time = self._cur_animation_time + self._animation_time_delta
-        self._prev_log_time = self._cur_animation_time
+        self._generation_time_delta = 1.0 / args.animation_rate
+        self._cur_generation_time = time.time()
+        self._next_generation_time = self._cur_generation_time + self._generation_time_delta
+        self._prev_log_time = self._cur_generation_time
         self._log_counter = 0
 
         self._head_generators = []
@@ -32,26 +31,31 @@ class PatternGenerator:
 
         self._LOG_RATE = 1.0
 
-    def results(self):
-        return self._results
-
-    async def loop(self):
-        self._cur_animation_time = self._next_animation_time
-        self._next_animation_time = self._cur_animation_time + self._animation_time_delta
-
-        # Skip a frame if falling too far behind
-        if time.time() > self._next_animation_time:
-            return
-        
+    async def generate_patterns(self):
         results = {}
         for generator in self._head_generators:
             head_id = generator.head_id()
-            segments = await generator.loop(self._animation_time_delta) 
+            segments = await generator.loop(self._generation_time_delta) 
             results[head_id] = self.Results(head_id, segments)
 
         # Update results future for processing by IO
         self._results.set_result(results)
         self._results = asyncio.Future()
+
+
+    def results(self):
+        return self._results
+
+    async def loop(self):
+        self._cur_generation_time = self._next_generation_time
+        self._next_generation_time = self._cur_generation_time + self._generation_time_delta
+
+        # Skip a frame if falling too far behind
+        if time.time() > self._next_generation_time:
+            return
+        
+        # The main work happens here
+        await self.generate_patterns()
 
         # Output update rate to console
         self._log_counter += 1
@@ -63,7 +67,7 @@ class PatternGenerator:
             self._prev_log_time = cur_log_time
 
         # Sleep for the remaining time
-        await asyncio.sleep(max(0, self._next_animation_time - time.time()))
+        await asyncio.sleep(max(0, self._next_generation_time - time.time()))
 
     async def run(self):
         for generator in self._head_generators:
--- light_controller/light_controller.py
+++ light_controller/light_controller.py
@@ -30,54 +30,33 @@ async def main():
                         help="The light controler WebSockets port.")
     parser.add_argument("--websockets_host", default="0.0.0.0", 
                         help="The light controler WebSockets host.")
-    parser.add_argument("--enable_launchpad", action='store_true', 
-                        help="Enables support for a USB launchpad device")
-    parser.add_argument("--ws_port_launchpad", type=int, default=5679, 
-                        help="The WebSockets port for the launchpad server")
-    parser.add_argument("--pattern_rotation_time", type=int, default=600, 
-                        help="The maximum duration a pattern is displayed before rotating to the next.")
-    parser.add_argument("--enable_pattern_mix_publisher", action='store_true', 
-                        help="Enables a WebSockets server to publish the pattern mix")
-    parser.add_argument("--pattern_mix_publish_port", type=int, default=5680, 
-                        help="The WebSockets port for the pattern mix publisher")
-    parser.add_argument("--enable_pattern_mix_subscriber", action='store_true', 
-                        help="Enables a WebSockets client to subscribe to a pattern mix")
-    parser.add_argument("--pattern_mix_subscribe_uri", default='ws://funkypi.wlan:5680', 
-                        help="The WebSockets URI for the pattern mix subscriber")
 
     args = parser.parse_args()
 
     config = json.load(args.config)
     head_configs = HeadConfigs(config["heads"])
-    
-    futures = []
+    state = InstallationState(config)
+
+    tasks = []
+
+    updater = StateUpdater(state, config)
+    tasks.append(updater.run())
 
     # Start pattern generator
-    pattern_generator = PatternGenerator(None, head_configs, args)
-    futures.append(pattern_generator.run())
+    pattern_generator = PatternGenerator(state, head_configs, args)
+    tasks.append(pattern_generator.run())
     
     # WS servers for the web visualization
     ws = LightControllerWebSocketsServer(pattern_generator, args.websockets_host, args.websockets_port)
-    futures.append(ws.run())
+    tasks.append(ws.run())
   
     opc = OpenPixelControlConnection(pattern_generator, head_configs)
-    futures.append(opc.run())
-
-    for o in config['heads']:
-        object_id = o['id']
-        if 'imu' in o.keys():
-            imu = o['imu']
-            url = "ws://" + imu['server_ip'] + ":" + str(imu['server_port'])
-            channel = imu['channel'] 
-            pattern_selector = pattern_generator.pattern_selectors[object_id]
-            pattern_selector.imu_orientation_channel = channel
-            # Start IMU websocket client
-            futures.append(pattern_selector.orientationWSListener(url))
+    tasks.append(opc.run())
 
     # Wait forever
     try:
         results = await asyncio.gather(
-            *futures,
+            *tasks,
             return_exceptions=False
         )
         print(results)
