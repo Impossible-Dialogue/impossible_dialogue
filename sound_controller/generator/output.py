2028a7d1f374e4b18be48a49a1fe0213d1a4f3e5
Ben Pitzer <ben.pitzer@gmail.com>
2024-08-17 21:12:29 -0700

    Add stereo output

--- sound_controller/generator/output.py
+++ sound_controller/generator/output.py
@@ -16,18 +16,28 @@ class Output:
 
         self._multiplex = config.multiplex
         self._output_streams = []
+        self._input_streams = []
+        self._multiplexers = []
 
         if self._multiplex:
             output_stream = Stream(queue_size=config.queue_size, channels=channels,
                                    blocksize=blocksize, dtype=dtype)
             self._output_streams.append(output_stream)
-            self._multiplexer = MultiChannelMixer(
+            self._multiplexers.append(MultiChannelMixer(
                 num_input_streams=state.num_heads(), output_stream=output_stream, channels=channels,
-                blocksize=blocksize, dtype=dtype)
+                blocksize=blocksize, dtype=dtype))
             self._pyaudio_task = asyncio.create_task(
                 pyaudio_output(output_stream, device_index=None, samplerate=sample_rate, channels=channels, blocksize=blocksize, dtype=dtype))
+        else:
+            self._input_streams = [Stream(channels=channels, blocksize=blocksize, dtype=dtype)
+                                   for i in range(state.num_heads())]
+            for i in range(state.num_heads() / 2):
+                self._pyaudio_task = asyncio.create_task(
+                    pyaudio_output_stereo(self._input_streams[2 * i], self._input_streams[2 * i + 1], device_index=i, samplerate=sample_rate, channels=channels, blocksize=blocksize, dtype=dtype))
 
 
     def input_stream(self, index):
         if self._multiplex:
             return self._multiplexer.input_stream(index)
+        else:
+            self._input_streams[index]
--- sound_controller/tasks/pyaudio.py
+++ sound_controller/tasks/pyaudio.py
@@ -38,3 +38,38 @@ async def pyaudio_output(input_stream, device_index=None, samplerate=48000, chan
 
     # Release PortAudio system resources.
     pa.terminate()
+
+
+async def pyaudio_output_stereo(input_stream1, input_stream2, device_index=None, samplerate=48000, channels=2, blocksize=512, dtype='float32'):
+    """Generator that streams blocks of audio data to a PyAudio output stream."""
+
+    def callback(in_data, frame_count, time_info, status):
+        assert frame_count == blocksize
+        try:
+            data1 = input_stream1.get_nowait()
+            data2 = input_stream2.get_nowait()
+        except asyncio.QueueEmpty:
+            logging.debug('Buffer is empty: increase buffersize?')
+            data = np.zeros((blocksize, channels), dtype=dtype)
+        data = np.concatenate((data1[0,:], data2[1,:]))
+        return (data, pyaudio.paContinue)
+
+    output_stream = pa.open(
+        format=pyaudio.paFloat32,
+        channels=channels,
+        rate=samplerate,
+        output=True,
+        output_device_index=device_index,
+        frames_per_buffer=blocksize,
+        stream_callback=callback
+    )
+
+    # Wait for stream to finish.
+    while output_stream.is_active():
+        await asyncio.sleep(1.0)
+
+    # Close the stream.
+    output_stream.close()
+
+    # Release PortAudio system resources.
+    pa.terminate()
