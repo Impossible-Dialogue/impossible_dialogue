6888c43f263502adbf39847ff9095796d053ac8f
Ben Pitzer <ben.pitzer@gmail.com>
2024-08-20 01:15:04 -0700

    More details

--- light_controller/orientation.py
+++ light_controller/orientation.py
@@ -17,11 +17,39 @@ def hello():
             message = websocket.recv()
             orientation_x = 360.0 - float(message)
 
+            websocket.send("imu/orientation_y")
+            message = websocket.recv()
+            orientation_y = 360.0 - float(message)
+
+            websocket.send("imu/orientation_z")
+            message = websocket.recv()
+            orientation_z = 360.0 - float(message)
+
             websocket.send("imu/head_orientation")
             message = websocket.recv()
             head_orientation = float(message)
 
-            print(f"orientation_x: {orientation_x}, head_orientation: {head_orientation}")
+            websocket.send("imu/calibration_sys")
+            message = websocket.recv()
+            calibration_sys = int(message)
+
+            websocket.send("imu/calibration_gyro")
+            message = websocket.recv()
+            calibration_gyro = int(message)
+            
+            websocket.send("imu/calibration_accel")
+            message = websocket.recv()
+            calibration_accel = int(message)
+            
+            websocket.send("imu/calibration_mag")
+            message = websocket.recv()
+            calibration_mag = int(message)
+
+            websocket.send("imu/timestamp")
+            message = websocket.recv()
+            imu_timestamp = int(message)
+
+            print(f"t: {imu_timestamp}, x: {orientation_x}, y: {orientation_y}, z: {orientation_z}, head: {head_orientation}, sys: {calibration_sys}, mag: {calibration_mag}, gyro: {calibration_gyro}")
             time.sleep(1/10.0)
 
 hello()
