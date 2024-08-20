e1038c00de3f9773221952d37f5cffee89193ff0
Ben Pitzer <ben.pitzer@gmail.com>
2024-08-20 01:14:29 -0700

    Update to opc

--- light_controller/count_leds.py
+++ light_controller/count_leds.py
@@ -1,28 +1,34 @@
 import sys
-from funky_lights import connection, messages
+from opclib import opc
 import numpy as np
 
-NUM_LEDS = 230
 
+NUM_LEDS = 500
+HEAD_1_ADDRESS = '10.10.3.15:7890'
+HEAD_2_ADDRESS = '10.10.3.12:7890'
+HEAD_3_ADDRESS = '10.10.3.13:7890'
+HEAD_4_ADDRESS = '10.10.3.14:7890'
+HEAD_5_ADDRESS = '10.10.3.16:7890'
+HEAD_6_ADDRESS = '10.10.3.10:7890'
+FIRE_PIT_ADDRESS = '10.10.3.11:7890'
 
 def main():
-    tty_device = connection.DEFAULT_TTY_DEVICE
-    uid = messages.BROADCAST_UID
-    baudrate = connection.LED_BAUDRATE
-
+    # Create a client object
+    client = opc.Client(FIRE_PIT_ADDRESS)
     if len(sys.argv) > 1:
-        tty_device = sys.argv[1]
-    if len(sys.argv) > 2:
-        baudrate = int(sys.argv[2])
-    if len(sys.argv) > 3:
-        uid = int(sys.argv[3])
-    if len(sys.argv) > 4:
-        count = int(sys.argv[4])
+        count = int(sys.argv[1])
     else:
         count = 20
 
-    serial_port = connection.InitializeController(
-        tty_device, baudrate=baudrate)
+
+    # Test if it can connect (optional)
+    if client.can_connect():
+        print('connected to %s' % FIRE_PIT_ADDRESS)
+    else:
+        # We could exit here, but instead let's just print a warning
+        # and then keep trying to send pixels in case the server
+        # appears later
+        print('WARNING: could not connect to %s' % ADDRESS)
 
     # Send messages to all the bars
     while True:
@@ -32,10 +38,8 @@ def main():
         for i in range(count - 1):
             rgbs[i, :] = (255, 255, 255)
         rgbs[count - 1, :] = (255, 0, 0)
-        serial_port.write(messages.PrepareLedMsg(uid, rgbs))
+        client.put_pixels(rgbs, channel=1)
         count = int(input("Number of LEDs: "))
-
-    serial_port.close()
 
 
 if __name__ == '__main__':
